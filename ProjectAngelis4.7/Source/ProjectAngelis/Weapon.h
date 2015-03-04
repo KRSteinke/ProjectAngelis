d3fefd1561f8ead82c09dcaf271bf7912459a1c8
sgjkbd <sgjkbd@mail.missouri.edu>
2015-03-04 16:34:01 -0600

    Revert "Back to 4.6. Character can strafe, but it is jittery. Added currency to character. Next I will make it so characters drop their weapons when they die."
    This reverts commit 76eca8c9bd128c7c5f8fea99bec9956b1ec3a7f6.

---
+++ ProjectAngelis4.7/Source/ProjectAngelis/Weapon.h
@@ -0,0 +1,124 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "Rocket.h"
+#include "Enemy.h"
+#include "Weapon.generated.h"
+
+class AWeaponEssentialsCharacter;
+
+#define TRACE_WEAPON ECC_GameTraceChannel1
+
+/**
+*
+*/
+
+
+
+UENUM(BlueprintType)
+namespace EWeaponProjectile
+{
+	enum ProjectileType
+	{
+		EBullet		UMETA(DisplayName = "Bullet"),
+		ESpread		UMETA(DisplayName = "Spread"),
+		EProjectile UMETA(DisplayName = "Projectile"),//possible syntax error
+	};
+
+}
+
+USTRUCT(BlueprintType)
+struct FWeaponData
+{
+	GENERATED_USTRUCT_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Ammo)
+	int32 MaxAmmo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	int32 MaxClip;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	float TimeBetweenShots;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Ammo)
+	int32 ShotCost;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	float WeaponRange;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	float WeaponSpread;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	FString Name;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	UTexture2D* SplashArt;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	int32 Priority;
+};
+
+UCLASS()
+class PROJECTANGELIS_API AWeapon : public AActor
+{
+
+	GENERATED_BODY()
+
+	void AttachToPlayer(); 
+	void DetachFromPlayer();
+
+public:
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	int32 CurrentAmmo;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	int32 CurrentClip;
+
+	AWeapon(const class FPostConstructInitializeProperties& PCIP);	
+
+	UFUNCTION()
+	void Instant_Fire();
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Config)
+	TEnumAsByte<EWeaponProjectile::ProjectileType> ProjectileType;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Collision)
+	TSubobjectPtr<class UBoxComponent> CollisionComp;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Config)
+	TSubobjectPtr<USkeletalMeshComponent> WeaponMesh;
+
+	UFUNCTION()
+	void Fire();
+
+	UPROPERTY(EditDefaultsOnly, Category = Config)
+	FWeaponData WeaponConfig;
+		
+	UFUNCTION()
+	virtual void ProjectileFire();	
+
+	UPROPERTY(EditDefaultsOnly, Category = Projectile)
+	TSubclassOf<class ARocket> ProjectileClass;
+
+	void OnEquip();
+	void OnUnEquip();
+
+	void SetOwningPawn(AWeaponEssentialsCharacter *NewOwner);
+
+	void ReloadAmmo();
+
+protected:
+	FHitResult WeaponTrace(const FVector &TraceFrom, const FVector &TraceTo) const;
+
+	void ProcessInstantHit(const FHitResult &Impact, const FVector &Origin, const FVector &ShootDir, int32 RandomSeed, float ReticleSpread);
+
+	AWeaponEssentialsCharacter *MyPawn;
+
+};
+
+
