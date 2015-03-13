ebe37c1ee21a65d9272459815fcaf43b160e28c1
nklwd2 <nklwd2@mail.missouri.edu>
2015-03-13 10:53:49 -0500

    added usableActor

---
+++ ProjectAngelis/Source/ProjectAngelis/UsableActor.h
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/Actor.h"
+#include "UsableActor.generated.h"
+
+UCLASS()
+class PROJECTANGELIS_API AUsableActor : public AActor
+{
+	GENERATED_UCLASS_BODY()
+	
+	UFUNCTION(BlueprintImplementableEvent)
+		bool OnUsed(ACharacter* character);
+
+	UFUNCTION(BlueprintImplementableEvent)
+		bool StartFocusItem();
+
+	UFUNCTION(BlueprintImplementableEvent)
+		bool EndFocusItem();
+	
+	
+};
