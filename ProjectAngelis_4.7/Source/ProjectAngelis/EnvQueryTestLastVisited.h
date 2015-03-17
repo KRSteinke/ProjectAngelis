62614702391dc090b742ad1c6d0999100bdd1a4e
Kurt Steinke <nuclear-cheese@hotmail.com>
2015-03-17 17:01:12 -0500

    Added an additional AIGuard into AI_Battleground, Fixed the EnemyAI not approaching/attacking player, added a rudimentary search patrol using EQS, currently has bug causing AI to choose same two points sometimes

---
+++ ProjectAngelis_4.7/Source/ProjectAngelis/EnvQueryTestLastVisited.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "EnvironmentQuery/EnvQueryTest.h"
+#include "EnvQueryTestLastVisited.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTANGELIS_API UEnvQueryTestLastVisited : public UEnvQueryTest
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
